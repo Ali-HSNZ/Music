ade9ecb0da30a30f5cc3681eefef8f7f0e5c7420
Ali Hassanzadeh <aliatraby@gmail.com>
2023-03-02 13:25:31 +0330

    extend tailwind Colors =>  tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+
     extend: {
+      colors : {
+        mainBgColor : "#1E1E1E",
+        secondBgColor : "#33373B",
+        yellow : "#FACD66",
+        mainColor : "#EFEEE0",
+        secondColor : "#ffffff80",
+        transparent : "transparent"
+      },
       fontFamily : {
         'quicksand-bold' : ['quicksand-bold'],
         'quicksand-light' : ['quicksand-light'],
