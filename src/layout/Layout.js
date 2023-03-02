862a621e82f887a38e159dffce4f3b5ed836a0ee
Ali Hassanzadeh <aliatraby@gmail.com>
2023-03-02 14:02:35 +0330

    Remove Header Component  Parent in Layout => src/layout/Layout.js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,12 +1,11 @@
 import Header from './Header.js'
+import Menu from './Menu.js'
 const Layout = ({children}) => {
     return(
         <div className="w-full">
-                <Header/>
-            <main className='w-full flex justify-between gap-x-4'>
-                <aside className='h-auto bg-gray-200'>
-                    <h1 className='text-white'>Jelow</h1>
-                </aside>
+            <Header/>
+            <main className='w-full flex justify-between  mt-6'>
+                <Menu/>
                 {children}
             </main>
        </div> 
