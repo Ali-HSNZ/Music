1c54c0b3baedaa308dc5827d88f5ad77a88fab97
Ali Hassanzadeh <aliatraby@gmail.com>
2023-03-02 17:25:29 +0330

    Change Menu Styles - change 'rounded' =>  src/layout/Menu.js

--- src/layout/Menu.js
+++ src/layout/Menu.js
@@ -7,7 +7,7 @@ import { IoLogOut } from 'react-icons/io5';
 const Menu = () => {
     return (
         <aside className='h-auto px-3 '>
-            <div className="bg-black p-3 rounded-lg">
+            <div className="bg-black p-3 py-5 rounded-full">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
                 </svg>
@@ -21,7 +21,7 @@ const Menu = () => {
                     <BsCollectionPlayFill className='text-secondColor h-6 w-6'/>
                 </div>
             </div>
-            <div className="bg-black mt-6 p-3 rounded-lg">
+            <div className="bg-black mt-6 p-3 py-5 rounded-full">
                 <div className='cursor-pointer'>
                     <RiUser3Fill className='text-secondColor h-6 w-6'/>
                 </div>
