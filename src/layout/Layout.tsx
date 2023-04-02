fcf1822e908bea8f7b038651e6420583eed7b0bd
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-02 20:28:01 +0430

    Responsived Layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -9,11 +9,11 @@ const Layout = (props : any) : JSX.Element => {
           <main className='w-full flex flex-col items-center justify-center pb-24'>
                <Header/>
 
-               <main className='h-auto w-full grid grid-cols-9 gap-x-6 max-w-[1600px]'>
+               <main className='h-auto w-full grid grid-cols-1 lg:grid-cols-9 gap-x-6 max-w-[1600px]'>
                     <Sidebar/>
 
                     {/* Main Content */}
-                    <main className='px-6 col-span-7 text-second h-auto'>
+                    <main className='px-6 col-span-7 h-auto'>
                          {props.children}
                     </main>
                </main>
