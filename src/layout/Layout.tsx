357e452edf0e988278a481c2276538cb3a48f732
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-06 15:33:15 +0430

    Deleted 'Player' Component in Layout App => src/layout/Layout.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,10 +1,13 @@
-import React, { ReactElement } from "react";
 import Header from "@layout/Header";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
-import Player from "./Player";
+import React from "react";
 
-const Layout = (props : any) : JSX.Element => {
+type TLayoutProps = {
+     children : JSX.Element
+}
+
+const Layout : React.FunctionComponent<TLayoutProps> = ({children}) : JSX.Element => {
      return (  
           <main className='w-full flex flex-col items-center justify-center pb-24'>
                <Header/>
@@ -14,10 +17,9 @@ const Layout = (props : any) : JSX.Element => {
 
                     {/* Main Content */}
                     <main className='px-6 col-span-7 h-auto'>
-                         {props.children}
+                         {children}
                     </main>
                </main>
-               <Player/>
                <Footer/>
           </main>
      );
