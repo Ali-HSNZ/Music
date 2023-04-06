ec0d235f48394bf33a230e44e8cb2170bcdf857a
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-06 23:46:59 +0430

    Set Home Link in SideBar Component => src/layout/Sidebar.tsx

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -3,17 +3,18 @@ import { BsCollectionPlayFill } from 'react-icons/bs';
 import { MdRadio } from 'react-icons/md';
 import { RiUser3Fill } from 'react-icons/ri';
 import { IoLogOut } from 'react-icons/io5';
+import Link from 'next/link';
 
 const Sidebar = () => {
      return (  
           <aside className='hidden lg:block col-span-2 sticky h-screen top-6 left-6 w-full z-30'>
                <div className="rounded-md overflow-hidden border border-secondBg">
-                    <div className='p-4 flex gap-x-2 items-center bg-secondBlackBg hover:bg-mainBlackBg'>
+                    <Link href="/" className='p-4 flex gap-x-2 items-center bg-secondBlackBg hover:bg-mainBlackBg'>
                          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
                               <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
                          </svg>
                          <span className='font-quicksand-medium text-yellow'>Home</span>
-                    </div>
+                    </Link>
                     <div className='p-4 cursor-pointer flex gap-x-2 bg-secondBlackBg hover:bg-mainBlackBg'>
                          <BsCollectionPlayFill className='text-second h-6 w-6'/>
                          <span className='font-quicksand-medium text-second'>Playlist</span>
