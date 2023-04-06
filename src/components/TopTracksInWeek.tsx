dacab335ca83723690202e3754c42b456e5928ec
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-06 23:44:12 +0430

    Set Singer Link  And Song Link in TopTracksInWeek Component

--- src/components/TopTracksInWeek.tsx
+++ src/components/TopTracksInWeek.tsx
@@ -5,6 +5,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { FreeMode , Navigation} from "swiper";
 import "swiper/css/free-mode";
+import Link from "next/link";
 //! <====Swiper====
 
 const TopWeekMusics = () => {
@@ -40,18 +41,18 @@ const TopWeekMusics = () => {
                                                        }
                                                             />
                                                   </div>
-                                                  <div className='my-3 w-fit relative pl-6 flex items-center justify-start'>
+                                                  <Link href={'/singer'} className='my-3 w-fit relative pl-6 flex items-center justify-start'>
                                                        <svg className="absolute text-second left-0 w-6 h-6 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                                                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                                                        </svg>
-                                                       <p className='w-full underline underline-offset-2 hover:no-underline cursor-pointer truncate ml-2 text-secondColor text-left font-quicksand-medium text-second'>Reza Pishro</p>
-                                                  </div>
-                                                       <button className=' p-2 w-full relative pl-11 flex items-center justify-start border group border-secondBg duration-150 hover:border-second h-auto rounded-md '>
-                                                            <svg className="absolute text-second left-3 w-6 h-6 "  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
-                                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
-                                                            </svg>
-                                                            <p className="truncate font-quicksand-regular text-main">Qabil</p>
-                                                       </button>
+                                                       <p className='w-full underline underline-offset-2 hover:no-underline cursor-pointer truncate ml-2 text-secondColor text-left font-quicksand-medium text-second'>Shayea</p>
+                                                  </Link>
+                                                  <Link href={'/song'} className='bg-secondBg bg-opacity-25 p-2 w-full relative pl-11 flex items-center justify-start border group border-secondBg duration-150 hover:border-second h-auto rounded-md '>
+                                                       <svg className="absolute text-second left-3 w-6 h-6 "  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
+                                                       </svg>
+                                                       <p className="truncate font-quicksand-regular text-main">Injaneb</p>
+                                                  </Link>
                                              </article>
                                         </div>
                               </SwiperSlide>
