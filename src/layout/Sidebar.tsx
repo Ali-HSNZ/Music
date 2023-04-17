6785814b419c06c746e684beead700a8a46f67a2
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-18 01:56:05 +0430

    Add/Set Link in SideBar TItle => src/layout/Sidebar.tsx

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -4,21 +4,25 @@ import { MdRadio } from 'react-icons/md';
 import { RiUser3Fill } from 'react-icons/ri';
 import { IoLogOut } from 'react-icons/io5';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Sidebar = () => {
+
+     const router = useRouter()
+
      return (  
           <aside className='hidden lg:block col-span-2 sticky h-screen top-6 left-6 w-full z-30'>
                <div className="rounded-md overflow-hidden border border-secondBg">
-                    <Link href="/" className='p-4 flex gap-x-2 items-center bg-secondBlackBg hover:bg-mainBlackBg'>
-                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+                    <Link href="/" className={`p-4 ${router.pathname === '/' ? "text-yellow" : "text-second"} flex gap-x-2 items-center bg-secondBlackBg hover:bg-mainBlackBg`}>
+                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6  cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
                               <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
                          </svg>
-                         <span className='font-quicksand-medium text-yellow'>Home</span>
+                         <span className='font-quicksand-medium '>Home</span>
                     </Link>
-                    <div className='p-4 cursor-pointer flex gap-x-2 bg-secondBlackBg hover:bg-mainBlackBg'>
-                         <BsCollectionPlayFill className='text-second h-6 w-6'/>
-                         <span className='font-quicksand-medium text-second'>Playlist</span>
-                    </div>
+                    <Link href={'/popular-playlists'} className={`p-4 ${router.pathname === '/popular-playlists' ? "text-yellow" : "text-second"} flex gap-x-2 items-center bg-secondBlackBg hover:bg-mainBlackBg`}>
+                         <BsCollectionPlayFill className='h-6 w-6'/>
+                         <span className='font-quicksand-medium '>Playlist</span>
+                    </Link>
                     <div className='p-4 cursor-pointer flex gap-x-2 bg-secondBlackBg hover:bg-mainBlackBg'>
                          <MdRadio className='text-second h-6 w-6'/>
                          <span className='font-quicksand-medium text-second'>Radio</span>
